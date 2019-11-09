3c45f5582ae900eccb553dfef855ae0dca765129
GDRadio <thegdradio@gmail.com>
2019-11-09 15:06:51 +0700

    Extended the user command

--- commands/user.js
+++ commands/user.js
@@ -1,8 +1,20 @@
 const Discord = require("discord.js")
 
-function userBot(user) {
+function isUserBot(user) {
 	if (user.bot) return "Да";
 	else return "Нет";
+}
+
+function userGame(member) {
+	if (member.presence.game) return member.presence.game.toString();
+	else return "ничего";
+}
+
+function userStatus(member) {
+	if (member.presence.status == "online") return "Онлайн";
+	else if (member.presence.status == "idle") return "Не активен";
+	else if (member.presence.status == "dnd") return "Не беспокоить";
+	else if (member.presence.status == "offline") return "Офлайн/невидимый";
 }
 
 module.exports = {
@@ -24,12 +36,17 @@ module.exports = {
 
 		const embed = new Discord.RichEmbed()
 			.setAuthor(user.tag, user.avatarURL || user.defaultAvatarURL)
-			.setColor("RANDOM")
-			.addField("Имя на этом сервере:", member.nickname)
-			.addField("Играет в:", "ничего" || member.presence.game.toString())
+			.setColor("RANDOM");
+		if (member.nickname)
+			embed.addField("Имя на этом сервере:", member.nickname)
+		embed
+			.addField("Статус:", userStatus(member))
+			.addField("Играет в:", userGame(member))
 			.addField("ID:", user.id)
+			.addField("Роли на этом сервере:", Array.from(member.roles.values()).join(", "))
 			.addField("Зарегистрировался:", user.createdAt)
-			.addField("Бот?", userBot(user));
+			.addField("Вошел на этот сервер:", member.joinedAt)
+			.addField("Бот?", isUserBot(user));
 	
 		await msg.channel.send(embed);
 	}
