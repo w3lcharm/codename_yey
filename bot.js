f76302ac48d30bafa51b9d68da35d7f1c7e67d2d
GDRadio <thegdradio@gmail.com>
2019-11-14 23:28:36 +0700

    Fix

--- bot.js
+++ bot.js
@@ -41,7 +41,7 @@ fs.readdirSync("./commands").filter(file => file.endsWith(".js")).forEach(file =
 function onReady() {
 	console.log(`${client.user.username} online!`);
 	client.user.setActivity(`${prefix}help`, { type: "PLAYING" });
-	client.db.run("create table warns('id' integer primary key autoincrement not null, server, user, moderator, reason)");
+	client.db.run("create table if not exists warns('id' integer primary key autoincrement not null, server, user, moderator, reason)");
 }
 
 async function onMessage(msg) {
