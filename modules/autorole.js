3138c4e24c8a9efb57ee3089f264e988b749f538
GDRadio <thegdradio@gmail.com>
2019-12-03 21:05:40 +0700

    Fix

--- modules/autorole.js
+++ modules/autorole.js
@@ -2,7 +2,7 @@ function autorole(client, member) {
     client.db.get("select * from settings where server = ?", member.guild.id, async (err, row) => {
         if (!row || !row.autorole) return;
         if (member.guild.me.hasPermission("MANAGE_ROLES"))
-            member.addRole(row.role);
+            member.addRole(row.autorole);
     });
 }
 
