9734068fbdf1a25680e383dc10970d5419ac2a99
GDRadio <thegdradio@gmail.com>
2019-12-18 22:43:51 +0700

    Cleaned the eval command code

--- commands/eval.js
+++ commands/eval.js
@@ -1,12 +1,5 @@
 const Discord = require("discord.js");
 
-function clean(text) {
-	if (typeof(text) === "string")
-		return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
-	else
-		return text;
-}
-
 module.exports = {
 	name: "eval",
 	description: "Evaluates the JavaScript code.\nThis command can only be used by the bot owner.",
@@ -16,11 +9,13 @@ module.exports = {
 	async run(client, msg, args, prefix) {
 		args = msg.content.slice(prefix.length).split(/ +/)
 		args.shift();
-		for (let arg of args) arg.trim();
+
 		const code = args.join(" ");
 		let evaled = eval(code);
+
 		if (typeof evaled != "string")
 			evaled = require("util").inspect(evaled);
-		await msg.channel.send(clean(evaled), { code: "xl" })
+
+		await msg.channel.send(evaled, { code: "js" })
 	}
 };
