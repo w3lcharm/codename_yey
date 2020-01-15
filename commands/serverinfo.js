fc368997dffdb32463afead63ab0e0004fa322ff
GDRadio <thegdradio@gmail.com>
2020-01-15 20:46:42 +0700

    Improved the serverinfo command

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -6,19 +6,25 @@ module.exports = {
 	guildOnly: true,
 	async run(client, msg, args, prefix) {
 		let iconURL = msg.guild.iconURL;
-		if (msg.guild.icon.startsWith("a_"))
+		if (msg.guild.icon != null && msg.guild.icon.startsWith("a_"))
 			iconURL = iconURL.replace("jpg", "gif");
 
 		const embed = new Discord.RichEmbed()
 			.setTitle(msg.guild.name)
 			.setColor("RANDOM")
 			.setThumbnail(iconURL)
-			.addField("Owner:", msg.guild.owner)
-			.addField("ID:", msg.guild.id)
-			.addField("Voice region:", msg.guild.region)
-			.addField("Total members:", msg.guild.memberCount)
-			.addField("Total channels:", msg.guild.channels.size)
-			.addField("Total roles:", msg.guild.roles.size)
+			.addField("Owner:", msg.guild.owner.user.tag, true)
+			.addField("ID:", msg.guild.id, true)
+			.addField("Voice region:", msg.guild.region, true)
+			.addField("Members:", `**total** - ${msg.guild.memberCount}\n` +
+				`**online** - ${msg.guild.members.filter(p => p.presence.status == "online").size}\n` +
+				`**idle** - ${msg.guild.members.filter(p => p.presence.status == "idle").size}\n` +
+				`**DND** - ${msg.guild.members.filter(p => p.presence.status == "dnd").size}\n` +
+				`**offline** - ${msg.guild.members.filter(p => p.presence.status == "offline").size}`, true)
+			.addField("Channels:", `**categories** - ${msg.guild.channels.filter(c => c instanceof Discord.CategoryChannel).size}\n` +
+				`**text** - ${msg.guild.channels.filter(c => c instanceof Discord.TextChannel).size}\n` +
+				`**voice** - ${msg.guild.channels.filter(c => c instanceof Discord.VoiceChannel).size}\n`, true)
+			.addField("Roles count:", msg.guild.roles.size, true)
 			.addField("Created at:", msg.guild.createdAt);
 		await msg.channel.send(embed);
 	}
