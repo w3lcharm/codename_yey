e452f409f49f75949ca5262dcc29cae70a015fa4
GDRadio <thegdradio@gmail.com>
2020-01-19 12:23:40 +0700

    added queue command

---
+++ commands/queue.js
@@ -0,0 +1,21 @@
+const Discord = require("discord.js");
+
+module.exports = {
+	name: "queue",
+	description: "Shows the music queue.",
+	guildOnly: true,
+	async run(client, msg, args, prefix) {
+		const queue = client.musicQueue.get(msg.guild.id);
+		if (queue) {
+			const description = queue.songs.map(song, number => `${number}: **${song.title}**`).join("\n");
+
+			const embed = new Discord.MessageEmbed()
+				.setTitle("Queue")
+				.setDescription(description)
+				.setFooter("codename_yey", client.user.displayAvatarURL());
+			await msg.channel.send(embed);
+		} else {
+			return msg.reply("music is not playing now.");
+		}
+	}
+}
