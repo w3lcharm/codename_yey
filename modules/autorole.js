5f867da2130641652fed85236e728508019ff8fe
GDRadio <thegdradio@gmail.com>
2020-02-08 16:58:47 +0700

    Bugfix

--- modules/autorole.js
+++ modules/autorole.js
@@ -1,7 +1,9 @@
 module.exports = async member => {
-    const dbItem = await settings.findOne({ where: { server: member.guild.id } });
+	const dbItem = await settings.findOne({ where: { server: member.guild.id } });
 
-    if (!dbItem.autorole) return;
-    if (member.guild.me.hasPermission("MANAGE_ROLES"))
-        await member.roles.add(dbItem.autorole);
-}
+	if (dbItem) {
+		if (!dbItem.autorole) return;
+		if (member.guild.me.hasPermission("MANAGE_ROLES"))
+			await member.roles.add(dbItem.autorole);
+	}
+}p
