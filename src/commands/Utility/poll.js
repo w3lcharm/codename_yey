297d67a112de8b7971ff975f33ae9ba56bafbfec
1z3ngero <thegdradio@gmail.com>
2020-04-02 00:40:39 +0700

    Enhanced the poll command

--- src/commands/Utility/poll.js
+++ src/commands/Utility/poll.js
@@ -1,19 +1,24 @@
 module.exports = {
 	name: "poll",
 	group: "Utility",
-	description: "Creates a reaction poll.",
-	usage: "<question> <answer1> [answer2] [answer3]",
+	description: "Creates a reaction poll (up to 10 answers).",
+	usage: "<question> <answers>",
 	async run(client, msg, args, prefix) {
 		if (!args.length)
 			return msg.channel.createMessage(`> Usage: \`${prefix}${this.name} ${this.usage}\``);
 
-		const [ question, answer1, answer2, answer3 ] = args;
+		const [ question, ...answers ] = args;
+		const reactions = [ "🇦", "🇧", "🇨", "🇩", "🇪", "🇫", "🇬", "🇭", "🇮", "🇯" ];
 
-		if (!answer1 && !answer2)
+		if (!answers.length)
 			return msg.channel.createMessage("> :warning: There must be at least one answer in the poll.");
+
+		if (answers.length > 10)
+			return msg.channel.createMessage("> :x: Not more than 10 answers in the poll.");
 
 		const embed = {
 			title: question,
+			description: answers.map((a, i) => `${reactions[i]} - ${a}`).join("\n"),
 			color: Math.round(Math.random() * 16777216) + 1,
 			footer: {
 				text: `Started by ${msg.author.username}#${msg.author.discriminator}`,
@@ -21,21 +26,9 @@ module.exports = {
 			},
 		};
 
-		let description = `🇦 - ${answer1}`;
-		let reactionArray = [ "🇦" ];
-		if (answer2) {
-			description += `\n🇧 - ${answer2}`;
-			reactionArray.push("🇧");
-		}
-		if (answer3) {
-			description += `\n🇨 - ${answer3}`;
-			reactionArray.push("🇨");
-		}
-		embed.description = description;
+		const message = await msg.channel.createMessage({ embed: embed });
 
-                const message = await msg.channel.createMessage({ embed: embed });
-
-                for (let reaction of reactionArray)
-                        await message.addReaction(reaction);
+		for (let i = 0; i < answers.length; i++)
+			await message.addReaction(reactions[i]);
 	}
 };
