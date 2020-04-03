bdae419b306664b41d4cdc8504e4082b138621da
1z3ngero <thegdradio@gmail.com>
2020-04-03 14:14:04 +0700

    Handling the shard errors

--- src/main.js
+++ src/main.js
@@ -59,4 +59,8 @@ client.on("commandError", async (commandName, msg, error) => {
 	client.logger.error(`Error in command ${commandName}:\n${error.stack}`);
 });
 
+client.on("error", (error, id) => {
+	client.logger.error(`Error in shard ${id}:\n${error.stack}`);
+});
+
 client.connect();
