8c669ca85ce3e7fa0944634269971594340f5f21
1z3ngero <thegdradio@gmail.com>
2020-04-24 15:12:35 +0700

    improved the ud command

--- src/commands/Utility/ud.js
+++ src/commands/Utility/ud.js
@@ -18,8 +18,26 @@ module.exports = {
 		const resp = await fetch(`http://api.urbandictionary.com/v0/define?${params}`);
 		const data = (await resp.json()).list[0];
 
-		if (!data)
-			return msg.channel.createMessage(`> :x: Word not found.`);
+		if (!data) {
+			return msg.channel.createMessage({
+				embed: {
+					title: ":x: Word not found.",
+					color: 15158332,
+				},
+			});
+		}
+
+		if ((!data.definition.length || data.definition.length > 1980) ||
+			(!data.example.length || data.example.length > 1000)) {
+			return msg.channel.createMessage({
+				embed: {
+					title: ":x: I can't show this definition here.",
+					description: `But there is a link to this definition: [(click here)](${data.permalink})`,
+					color: 15158332,
+				},
+			});
+		}
+
 
 		const embed = {
 			title: data.word,
