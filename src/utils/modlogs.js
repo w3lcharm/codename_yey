6f2334fa5a98f92628386dc54c6b51b79e897c9b
1z3ngero <thegdradio@gmail.com>
2020-06-02 12:37:04 +0700

    Hotfix

--- src/utils/modlogs.js
+++ src/utils/modlogs.js
@@ -181,7 +181,7 @@ async function onMessageUpdate(client, newMsg, oldMsg) {
 
 	if (!newMsg.author) return;
 	if (newMsg.author.bot) return;
-	if (!oldMsg.embeds.length || !newMsg.embeds.length) return;
+	if (oldMsg.embeds.length || newMsg.embeds.length) return;
 
 	let oldContent = oldMsg.content;
 	let newContent = newMsg.content;
