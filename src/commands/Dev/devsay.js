fd54c2fd56480f65599caf1bbd251629a35b9c03
1z3ngero <thegdradio@gmail.com>
2020-06-15 19:05:12 +0700

    .

---
+++ src/commands/Dev/devsay.js
@@ -0,0 +1,17 @@
+module.exports = {
+  name: "devsay",
+  group: "devGroup",
+  description: "devsayDescription",
+  ownerOnly: true,
+  hidden: true,
+  async run(client, msg, args, prefix, lang) {
+    if (!args.length) return;
+
+    let text = msg.content.slice(preifx.length + this.name.length + 1);
+
+    try {
+      await msg.delete();
+      await msg.channel.createMessage(text);
+    } catch {};
+  }
+}
