fa06490d839f718f6cc2509e4602330419e51c59
1z3ngero <1z3ngero@gmail.com>
2020-09-25 09:40:38 +0700

    Added DBL extension

--- src/main.js
+++ src/main.js
@@ -51,6 +51,7 @@ client.on("commandError", async (commandName, msg, error, showErr, lang = client
 client.loadExtension(path.join(__dirname, "extensions/autorole.js"));
 client.loadExtension(path.join(__dirname, "extensions/modlogs.js"));
 client.loadExtension(path.join(__dirname, "extensions/welcomeMessages.js"));
+client.loadExtension(path.join(__dirname, "extensions/dbl.js"), config.dblApiKey);
   
 client.on("error", (error, id) => {
   client.logger.error(`Error in shard ${id}:\n${error.stack}`);
