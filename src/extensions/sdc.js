18fb45da9ba6fa1515f2927bea0a907aa6dbdcb9
1z3ngero <1z3ngero@gmail.com>
2020-09-27 11:39:03 +0700

    Fix typo

--- src/extensions/sdc.js
+++ src/extensions/sdc.js
@@ -11,10 +11,10 @@ async function postStats(client) {
   const response = await fetch(`${apiURL}/bots/${client.user.id}/stats`, {
     method: "POST",
     headers: { Authorization: apiKey, "Content-Type": "application/x-www-form-urlencoded" },
-    body: stringify({ shards: client.shards.size, servers: client.guids.size }),
+    body: stringify({ shards: client.shards.size, servers: client.guilds.size }),
   });
   const data = await response.json();
-  
+
   if (response.status != 200) {
     throw new Error(`${data.error.code} ${data.error.type}: ${data.error.message}`);
   }
