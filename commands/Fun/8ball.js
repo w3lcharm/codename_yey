04455c4ed134993f7c9f544bba23dbdc53aa0557
1z3ngero <1z3ngero@gmail.com>
2020-10-09 00:14:21 +0700

    Moved all 8ball logic to command file

--- commands/Fun/8ball.js
+++ commands/Fun/8ball.js
@@ -1,4 +1,43 @@
-const magicball = require("../../extensions/8ball");
+function getRandom(arr) {
+  return arr[Math.round(Math.random() * (arr.length - 1))];
+}
+
+function getNth(arr, n) {
+  return arr[Math.abs(n) % arr.length];
+}
+
+const life = 42;
+const vals = [ 4, 9, 6, 2, 5, 8, 1 ];
+
+const ansMapping = [
+  true, true, true, true, true, true, true, true,
+  true, true, false, false, false, false, false,
+];
+
+function hashCode(s) {
+  let h = 0, l = s.length, i = 0;
+
+  if (l > 0) {
+    while (i < l) {
+      h = (h << 5) - h + s.charCodeAt(i++) | 0;
+    }
+  }
+
+  return h;
+}
+
+function predict(question, failChance, seed, lang) {
+  const clQuestion = question.toLowerCase()
+    .replace(/[.,:;!?\s@#$%^&*()_+\-=]/g, "");
+
+  if (Math.random() <= failChance) {
+    return getRandom(lang._8ballFailAnswers);
+  }
+
+  const isPos = getNth(ansMapping, hashCode(clQuestion) + (seed *
+    Math.round(life / getNth(vals, seed))));
+  return getRandom(lang._8ballAnswers[isPos]);
+}
 
 module.exports = {
   name: "8ball",
@@ -13,7 +52,7 @@ module.exports = {
 
     const embed = {
       title: lang.magicballAnswer,
-      description: magicball.predict(question, 0.25, msg.author.id, lang.langName),
+      description: predict(question, 0.25, msg.author.id, lang),
       color: 9807270,
       fields: [
         {
