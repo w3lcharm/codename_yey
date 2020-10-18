6d53e0e2194615b0c2179f0d588c7286655d72b7
1z3ngero <1z3ngero@gmail.com>
2020-10-18 17:35:42 +0700

    changed the status

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ client.on("guildCreate", guild => client.logger.info(`New server: ${guild.name} 
   .on("guildDelete", guild => client.logger.info(`Left from server ${guild.name} (ID: ${guild.id})`));
 
 function editStatus() {
-  return client.editStatus({ name: `on ${client.guilds.size} servers | you can mention me to find out my prefix` });
+  return client.editStatus({ name: `on ${client.guilds.size} servers | type yey!help or @${client.user.username}` });
 }
 client.on("guildCreate", editStatus)
   .on("guildDelete", editStatus)
