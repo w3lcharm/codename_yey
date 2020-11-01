ba354287b732d71f6e497d25b0a362d0c5263717
1z3ngero <1z3ngero@gmail.com>
2020-11-01 22:50:54 +0700

    Added example config file #4

---
+++ config.example.js
@@ -0,0 +1,14 @@
+module.exports = {
+  prefix: "your prefix",
+  token: "your-token",
+  database: {
+    dialect: "sqlite", // or any other dialect that Sequelize supports
+    database: "database name", // this can be omitted if using sqlite
+    username: "username", // this too
+    password: "password", // and this
+    host: "localhost", // and this
+    storage: "./bot.db", // you should use that if you're using sqlite
+  },
+  owners: [ "insert-your-id-here" ],
+  supportChannelID: "id", // this is needed for support command
+};
