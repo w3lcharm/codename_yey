adcc512933d0672905b4ec2bfa277c56c58a58d7
1z3ngero <1z3ngero@gmail.com>
2020-11-01 14:22:13 +0700

    bruh

--- commands/Basic/kotletki.js
+++ commands/Basic/kotletki.js
@@ -4,6 +4,6 @@ module.exports = {
   description: "котлетки",
   hidden: true,
   async run(client, msg, args, prefix) {
-    await msg.channel.createMessage("котлетки"); // RGltdXRlcjA1INGI0LjQt9C+0LjQtA==
+    await msg.channel.createMessage("котлетки"); // 0Y3RgtGDINC/0LDRgdGF0LDQu9C60YMg0YHQv9C40LfQtNC40LvQuCDRhtGL0LPQsNC90LU=
   }
 };
