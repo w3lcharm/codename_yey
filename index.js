60f6645bce0c887cc7f4ef058fd33c47df4aa457
1z3ngero <1z3ngero@gmail.com>
2020-11-02 21:37:26 +0700

    Changed version

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ global.client = new CmdClient(config.token, {
   defaultImageSize: 2048,
 });
 
-client.version = "2.5.2";
+client.version = "2.5.3";
 
 const cmdGroups = [ "Basic", "Utility", "Moderation", "Fun", "Misc", "Settings", "Dev" ];
 for (const group of cmdGroups) {
