0f3d5856626c06292a7df914253efff4288ddba7
1z3ngero <1z3ngero@gmail.com>
2021-03-22 00:03:59 +0700

    removed joinmessage and leavemessage commands

--- dbModels/welcomeMessages.js
+++
@@ -1,7 +0,0 @@
-module.exports = (sequelize, DataTypes) => {
-  return sequelize.define("welcomeMessages", {
-    server: DataTypes.STRING,
-    channel: DataTypes.STRING,
-    message: DataTypes.TEXT,
-  }, { timestamps: false });
-}
