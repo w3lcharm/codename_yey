3efdcb8aecc2030b15da7958243153eda4fc769c
1z3ngero <1z3ngero@gmail.com>
2021-04-11 12:57:31 +0700

    another bugfix

--- core/CmdClient.js
+++ core/CmdClient.js
@@ -111,7 +111,7 @@ class CmdClient extends Eris.Client {
         let expiration = cmdCooldowns.get(msg.author.id) + (command.cooldown * 1000);
         if (now < expiration) {
           let secsLeft = Math.floor((expiration - now) / 1000);
-          return msg.reply(lang.cooldown(secsLeft));
+          return msg.reply(msg.t("cooldown", secsLeft));
         }
       }
     }
