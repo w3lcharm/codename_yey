dd18ba925fcacd52d5724dbb04579a7f7e2ce8d9
1z3ngero <53137848+1z3ngero@users.noreply.github.com>
2021-08-09 15:40:48 +0700

    котлетки

--- commands/General/kotletki.js
+++ commands/General/kotletki.js
@@ -4,6 +4,6 @@ module.exports = {
   description: "котлетки",
   hidden: true,
   async run(client, msg, args, prefix) {
-    await msg.reply("0LrQvtGC0LvQtdGC0LrQuCB+fkFsZGVyaXM/fn4="); // 0Y3RgtGDINC/0LDRgdGF0LDQu9C60YMg0YHQv9C40LfQtNC40LvQuCDRhtGL0LPQsNC90LU=
+    await msg.reply("котлетки"); // 0Y3RgtGDINC/0LDRgdGF0LDQu9C60YMg0YHQv9C40LfQtNC40LvQuCDRhtGL0LPQsNC90LU=
   }
 };
